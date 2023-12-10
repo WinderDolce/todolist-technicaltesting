2c160b8f4208beeaa4c808620dd11b1fc0c24ba5
Winder Dolce <58010177+WinderDolce@users.noreply.github.com>
2023-12-10 01:52:13 -0400

    mi primer commit

---
+++ src/layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>Todo list technical testing</header>
+
+  <hr />
+
+  <slot />
+
+  <hr />
+
+  <footer>Vue 3</footer>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss">
+header,
+footer {
+  text-align: center;
+  font-weight: bold;
+}
+</style>
